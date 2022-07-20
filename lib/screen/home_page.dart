afed47138af4f98c7e8df49d1e5cd4eda7db84a0
zhalgas <zhalgas.khassenov@buhta.com>
2022-07-20 09:51:19 +0600

    refactor: extracted widgets on home page

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -10,19 +10,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final infinitiveController = TextEditingController();
   final pastController = TextEditingController();
   final participleController = TextEditingController();
-  final translationController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
-    infinitiveController.dispose();
     pastController.dispose();
     participleController.dispose();
-    translationController.dispose();
     super.dispose();
   }
 
@@ -41,21 +37,17 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  TextStyle _style() {
-    return const TextStyle(
-      color: Colors.black,
-      fontStyle: FontStyle.normal,
-      fontSize: 20,
-    );
-  }
+  TextStyle? get _style => Theme.of(context).textTheme.titleLarge;
 
-  TextStyle _hintStyle() {
-    return TextStyle(
-      color: Colors.black.withOpacity(0.6),
-      fontSize: 20,
-      fontStyle: FontStyle.italic,
-    );
-  }
+  TextStyle? get _hintStyle => _style?.copyWith(
+        color: Colors.black.withOpacity(0.6),
+        fontStyle: FontStyle.italic,
+      );
+
+  InputDecoration get inputDecoration => InputDecoration(
+      border: const OutlineInputBorder(),
+      hintStyle: _hintStyle,
+      errorStyle: const TextStyle(height: 0));
 
   @override
   Widget build(BuildContext context) {
@@ -64,134 +56,124 @@ class _HomePageState extends State<HomePage> {
       return Scaffold(
           body: Center(
         child: Text(
-          "Loading,,,",
-          style: _style(),
+          "Loading...",
+          style: _style,
         ),
       ));
     } else {
+      final verb = VerbsListNotifier.currentVerbOf(context);
       return Scaffold(
-          appBar: AppBar(title: const Text("Irregular verbs app")),
-          bottomNavigationBar: const BottomNavbar(),
-          body: Builder(builder: (context) {
-            final verb = VerbsListNotifier.currentVerbOf(context);
-            return Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Card(
-                    child: Padding(
-                        padding: const EdgeInsets.all(35.0),
-                        child: Center(
-                          child: Form(
-                            key: _formKey,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Center(
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: <Widget>[
-                                      Expanded(
-                                          flex: 1,
-                                          child: Text(
-                                            "Infinitive:",
-                                            style: _style(),
-                                          )),
-                                      Expanded(
-                                          flex: 2,
-                                          child: Center(
-                                            child: Text(
-                                              verb.infinitive,
-                                              style: _style(),
-                                            ),
-                                          ))
-                                    ],
-                                  ),
-                                ),
-                                Center(
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: <Widget>[
-                                      Expanded(
-                                        flex: 1,
-                                        child: Text(
-                                          "Past:",
-                                          style: _style(),
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 2,
-                                        child: TextFormField(
-                                          validator: (input) {
-                                            return verb.past ==
-                                                    input?.toLowerCase()
-                                                ? null
-                                                : '';
-                                          },
-                                          decoration: InputDecoration(
-                                              border:
-                                                  const OutlineInputBorder(),
-                                              hintStyle: _hintStyle(),
-                                              hintText: 'Past'),
-                                          controller: pastController,
-                                          style: _style(),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Center(
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: <Widget>[
-                                      Expanded(
-                                        flex: 1,
-                                        child: Text(
-                                          "Participle:",
-                                          style: _style(),
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 2,
-                                        child: TextFormField(
-                                          validator: (input) {
-                                            return verb.participle ==
-                                                    input?.toLowerCase()
-                                                ? null
-                                                : '';
-                                          },
-                                          decoration: InputDecoration(
-                                              border:
-                                                  const OutlineInputBorder(),
-                                              hintStyle: _hintStyle(),
-                                              hintText: 'Participle'),
-                                          controller: participleController,
-                                          style: _style(),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Center(
-                                  child: Ink(
-                                    decoration: const ShapeDecoration(
-                                      color: Colors.lightBlue,
-                                      shape: CircleBorder(),
-                                    ),
-                                    child: IconButton(
-                                      icon: const Icon(Icons.navigate_next),
-                                      tooltip: 'Submit',
-                                      iconSize: 80,
-                                      onPressed: () => _onClick(context),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ))));
-          }));
+        appBar: AppBar(title: const Text("Irregular verbs app")),
+        bottomNavigationBar: const BottomNavbar(),
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(35.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ColumnCardTile(
+                      title: 'Infinitive',
+                      body: Center(
+                        child: Text(
+                          verb.infinitive,
+                          style: _style,
+                        ),
+                      ),
+                    ),
+                    ColumnCardTile(
+                      title: 'Past:',
+                      body: TextFormField(
+                        validator: (input) {
+                          return verb.past == input?.toLowerCase() ? null : '';
+                        },
+                        decoration: inputDecoration.copyWith(hintText: 'Past'),
+                        controller: pastController,
+                        style: _style,
+                      ),
+                    ),
+                    ColumnCardTile(
+                      title: 'Participle:',
+                      body: TextFormField(
+                        validator: (input) {
+                          return verb.participle == input?.toLowerCase()
+                              ? null
+                              : '';
+                        },
+                        decoration:
+                            inputDecoration.copyWith(hintText: 'Participle'),
+                        controller: participleController,
+                        style: _style,
+                      ),
+                    ),
+                    Center(
+                      child: SubmitButton(onPressed: () => _onClick(context)),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
     }
+  }
+}
+
+class ColumnCardTile extends StatelessWidget {
+  final String title;
+  final Widget body;
+
+  const ColumnCardTile({
+    Key? key,
+    required this.title,
+    required this.body,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).textTheme.titleLarge;
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: Text(
+          title,
+          style: style,
+        )),
+        Expanded(flex: 2, child: body)
+      ],
+    );
+  }
+}
+
+class SubmitButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const SubmitButton({
+    Key? key,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+      decoration: const ShapeDecoration(
+        color: Colors.lightBlue,
+        shape: CircleBorder(),
+      ),
+      child: IconButton(
+        onPressed: onPressed,
+        icon: const Icon(
+          Icons.navigate_next,
+          color: Colors.white,
+        ),
+        tooltip: 'Submit',
+        iconSize: 80,
+        // onPressed: () => _onClick(context),
+      ),
+    );
   }
 }
